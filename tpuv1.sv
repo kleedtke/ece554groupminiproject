1215f7155683be50f4041b9fa800928c70404d74
adam360x <61172547+adam360x@users.noreply.github.com>
2021-02-10 23:09:00 -0600

    Update tpuv1.sv

--- tpuv1.sv
+++ tpuv1.sv
@@ -3,8 +3,8 @@ module tpuv1
     parameter BITS_AB=8,
     parameter BITS_C=16,
     parameter DIM=8,
-    parameter ADDRW=16;
-    parameter DATAW=64;
+    parameter ADDRW=16,
+    parameter DATAW=64
     )
    (
     input clk, rst_n, r_w, // r_w=0 read, =1 write
@@ -13,4 +13,6 @@ module tpuv1
     input [ADDRW-1:0] addr
    );
     
+
+
 endmodule
