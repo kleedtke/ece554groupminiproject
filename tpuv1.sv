d2d4695184d6ccd0bb0ed8edd59a45ddcfa189be
adam360x <61172547+adam360x@users.noreply.github.com>
2021-02-10 23:24:32 -0600

    Update tpuv1.sv

--- tpuv1.sv
+++ tpuv1.sv
@@ -12,7 +12,36 @@ module tpuv1
     output [DATAW-1:0] dataOut,
     input [ADDRW-1:0] addr
    );
-    
+
+logic [BITS_AB-1:0] A [DIM-1:0];
+logic [BITS_AB-1:0] B [DIM-1:0];
+logic [BITS_C-1:0]  Cin [DIM-1:0];
+logic [$clog2(DIM)-1:0] Crow;
+logic [BITS_AB-1:0] A_memTo_array [DIM-1:0];
+logic [BITS_AB-1:0] B_memTo_array [DIM-1:0];
+
+   
+systolic_array
+#(
+   .BITS_AB(BITS_AB),
+   .BITS_C(BITS_C),
+   .DIM(DIM)
+   )
+  ourSystolic_array(
+   .clk(clk), .rst_n(rst_n), .WrEn(r_w), .en(1'b1),
+   .A(A_memTo_array), .B(B), .Cin(Cin),
+   .Crow(Crow), .Cout(dataOut)
+   );
+
+memA
+#(.BITS_AB(BITS_AB), .DIM(DIM))
+  ourMemA(
+	.clk(clk), .rst_n(rst_n), .WrEn(r_w), .en(1'b1),
+	.Ain(A), .Arow(1'b0), .Aout(A_memTo_array));
+
+memB
+#(.BITS_AB(BITS_AB), .DIM(DIM))
+  ourMemB(.clk(clk), .rst_n(rst_n), .en(1'b1), .Bin(B), .Bout(B_memTo_array));
 
 
 endmodule
